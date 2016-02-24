19673ab6bae68e6fa87d2874046f75086f553816
satanworker <satanworker@gmail.com>
2016-02-24 12:25:04 +0300

    clean freelance

---
+++ node/http_get.js
@@ -0,0 +1,5 @@
+/**
+ * Created by dmitry on 24.02.16.
+ */
+var http = require('http');
+//http.get(process.argv[2], function)
--- node/program.js
+++ node/program.js
@@ -2,9 +2,14 @@
  * Created by dmitry on 21.02.16.
  */
 var http = require('http');
+var bl = require('bl');
 http.get(process.argv[2], function(response) {
     response.setEncoding('utf-8');
-   response.on('data', function (data) {
-       console.log(data);
-   });
+    response.pipe(
+        bl(function(err, data) {
+            if(err) throw err;
+            console.log(data.toString().length);
+            console.log(data.toString());
+        })
+    );
 });
