38f35fc898c59eed925197e28d0eb56d5314d09e
satanworker <satanworker@gmail.com>
2016-03-19 19:19:27 +0300

    clean freelance

--- wild_new/js/app.js
+++ wild_new/js/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $('video').css({height: $('video').width()/1.7765625})
+    $('video').css({height: $('video').width()/1.7765625});
     $(window).scroll(function() {
        if(!$('.video_text').is('.active') && ($(this).scrollTop()+$(this).outerHeight() >= $('.video_scroll').offset().top)) {
             $('video')[0].play();
@@ -7,6 +7,17 @@ $(document).ready(function() {
                $('.video_text').addClass('active');
            }, 8000);
        }
+        if($(this).scrollTop()+55 > $('.row.first').outerHeight()) {
+            $('.header.fixed').removeClass('closed');
+        }
+        else {
+            $('.header.fixed').addClass('closed');
+        }
+    });
+    $('.scroll').click(function(event) {
+        event.preventDefault();
+        var target = $(this.hash).offset().top - 44;
+        $("html, body").animate({ scrollTop:  target}, 250);
     });
     $('[data-target]').click(function() {
        $(this).siblings().removeClass('active');
