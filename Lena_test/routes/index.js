79339344afb19e4ad8734166eb18e03b90bb0388
kilaz <sergeev.maxs@gmail.com>
2016-04-05 13:54:55 +0300

    phantomjs not working bitch

--- Lena_test/routes/index.js
+++ Lena_test/routes/index.js
@@ -7,36 +7,44 @@ var router = express.Router();
 
 
 
-var renderPdf = function(session, cb) {
-  var page;
-
-  try {
-    session.createPage(function(_page) {
-      page = _page;
-      // ...
-      var file = '/tmp/file.pdf';
-      page.render(file, function() {
-        page.close();
-        page = null;
-        return cb(null, file);
-      });
-    });
-  } catch(e) {
-    try {
-      if (page != null) {
-        page.close(); // try close the page in case it opened but never rendered a pdf due to other issues
-      }
-    } catch(e) {
-      // ignore as page may not have been initialised
-    }
-    return cb('Exception rendering pdf:' + e.toString());
-  }
-};
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  var phantom = require('node-phantom');
+  var html = ejs.render(htmlFileContent,
+      {seller: "Sakshi Tyagi", buyer: "Test Buyer"});
+  phantom.create(function (error, ph) {
+    ph.createPage(function (error, page) {
+      page.settings = {
+        loadImages: true,
+        localToRemoteUrlAccessEnabled: true,
+        javascriptEnabled: true,
+        loadPlugins: false
+      };
+      page.set('viewportSize', { width: 800, height: 600 });
+      page.set('paperSize', { format: 'A4', orientation: 'portrait', border: '1cm' });
+      page.set('content', html, function (error) {
+        if (error) {
+          console.log('Error setting content: ', error);
+        }
+      });
 
-  renderPdf(req)
+      page.onResourceRequested = function (rd, req) {
+        console.log("REQUESTING: ", rd[0]["url"]);
+      }
+      page.onResourceReceived = function (rd) {
+        rd.stage == "end" && console.log("LOADED: ", rd["url"]);
+      }
+      page.onLoadFinished = function (status) {
+        page.render(url, function (error) {
+          if (error) console.log('Error rendering PDF: %s', error);
+          console.log("PDF GENERATED : ", status);
+          ph.exit();
+          cb && cb();
+        });
+      }
+    });
+  });
 
   res.render('index', { title: 'Express' });
 
