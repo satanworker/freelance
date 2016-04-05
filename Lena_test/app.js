cb4d206621e4b62b3a7ca7f420591a306bf1b107
satanworker <satanworker@gmail.com>
2016-04-05 15:06:16 +0300

    angular continue

--- Lena_test/app.js
+++ Lena_test/app.js
@@ -4,15 +4,16 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var exphbs  = require('express-handlebars');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+//app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', exphbs({defaultLayout: 'single', extname: '.hbs'}));
+app.set('view engine', '.hbs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
--- Lena_test/routes/index.js
+++ Lena_test/routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var path = require('path');
 var slash = require('slash');
+var system = require('system');
 var phantomjs = require('phantomjs');
 var phantom = require('phantom');
 var router = express.Router();
@@ -10,54 +11,73 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var phantom = require('node-phantom');
-  var html = ejs.render(htmlFileContent,
-      {seller: "Sakshi Tyagi", buyer: "Test Buyer"});
-  phantom.create(function (error, ph) {
-    ph.createPage(function (error, page) {
-      page.settings = {
-        loadImages: true,
-        localToRemoteUrlAccessEnabled: true,
-        javascriptEnabled: true, 
-        loadPlugins: false
-      };
-      page.set('viewportSize', { width: 800, height: 600 });
-      page.set('paperSize', { format: 'A4', orientation: 'portrait', border: '1cm' });
-      page.set('content', html, function (error) {
-        if (error) {
-          console.log('Error setting content: ', error);
-        }
-      });
+  res.render('main',{title: 'test'});
 
-      page.onResourceRequested = function (rd, req) {
-        console.log("REQUESTING: ", rd[0]["url"]);
-      }
-      page.onResourceReceived = function (rd) {
-        rd.stage == "end" && console.log("LOADED: ", rd["url"]);
-      }
-      page.onLoadFinished = function (status) {
-        page.render(url, function (error) {
-          if (error) console.log('Error rendering PDF: %s', error);
-          console.log("PDF GENERATED : ", status);
+
+  //phantomg which works
+ /* var phantom = require('phantom');
+  phantom.create().then(function(ph)
+    ph.createPage().then(function(page) {
+      page.open("/").then(function(status) {
+        page.render('google.pdf').then(function() {
+          console.log('Page Rendered');
           ph.exit();
-          cb && cb();
         });
-      }
+      });
     });
   });
+*/
+  var PhantomPDF = require('phantom-pdf');
 
-  res.render('index', { title: 'Express' });
+  var manifest = {
+    phantomjsPath: 'node_modules/phantomjs/bin/phantomjs', // Optional
+    templates: {
+      body: __dirname+'/body.hbs', // Body is required as its the entry point
+      // If header is defined it will be the page header
+      // Note: phantomSettings.paperSize.header.height must also be set
+      //header: __dirname+'/templates/header.hbs',
+      // If footer is defined it will be the page header
+      // Note: phantomSettings.paperSize.footer.height must also be set
+      //footer: __dirname+'/templates/footer.hbs',
+      // This is an example of having a parcial view
+      //product: __dirname+'/templates/product.hbs'
+    },
+    //helpers: __dirname+'/helpers/index.js', // Handlebars helper
+    //helperVariables: {}, // Additional data to be passed in the helper such as local
+    //less: __dirname + '/less/index.less', // Less file to render into css
+    output: '/tmp/foo.pdf', // This is the destination of the newly created PDF
+    // Settings to be passed into phantom
+    // List of settings: http://phantomjs.org/api/webpage/
+    //phantomSettings: {
+    //  paperSize: {
+    //    format: 'Letter',
+    //    orientation: 'portrait',
+    //    margin: {
+    //      top: '0.25in',
+    //      right: '0.5in',
+    //      bottom: '0.25in',
+    //      left: '0.5in'
+    //    },
+    //    header: {
+    //      height: '0.5in'
+    //    },
+    //    footer: {
+    //      height: '0.5in'
+    //    }
+    //  }
+    //}
+  };
 
+  var data = { // Put any data you want to be exposed to your handlebars template
+    products: ['soccer ball', 'baseball', 'football'],
+    category: 'Balls'
+  };
+  var pdf = new PhantomPDF(manifest, data, function(err){
+    console.log(err)
+  });
 });
 
 router.get('/dlpdf', function(req, res, next) {
-  console.log('dlpdf');
-  /*page.render('/tmp/file.pdf', function() {
-    // file is now written to disk
-  });*/
-
-
-
   res.render('index', {title: 'Test'});
 });
 
